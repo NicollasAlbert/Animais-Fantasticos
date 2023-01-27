0f558a59db3f01ebfd433efe8934aea56d67b844
Nicollas <nicksz55@hotmail.com>
2023-01-27 18:10:14 -0300

    Alterações

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-// Adicione a classe ativo a todos os itens do menu
-const ativar = document.querySelectorAll('.menu a');
-ativar.forEach((item) => item.classList.add('ativo'));
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const imgDistancia = document.querySelector("img");
+console.log(imgDistancia.offsetTop);
+const imgRect = imgDistancia.getBoundingClientRect().top;
+console.log(imgRect);
 
-// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-ativar.forEach((item) => item.classList.remove('ativo'));
-ativar[0].classList.add('ativo');
+// Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const imgSoma = document.querySelectorAll("img");
+  let soma = 0;
+  imgSoma.forEach(function (img) {
+    const imgTotal = img.offsetWidth;
+    soma += img.offsetWidth;
+  });
+  console.log(soma);
+}
 
-// Verifique se as imagens possuem o atributo alt
-const imgs = document.querySelectorAll('img');
-imgs.forEach((img) => {
-  const possuiAtributo = img.hasAttribute('Alt');
-  console.log(img, possuiAtributo);
+window.onload = function () {
+  somaImagens();
+};
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+links.forEach((link) => {
+  linkWidth = link.offsetWidth;
+  linkHeight = link.offsetHeight;
+  if(linkWidth >= 48 && linkHeight >= 48)
+  console.log(link, 'Os links estão cumprindo o recomendado')
+  else
+  console.log(link, 'Os links não estão cumprindo o recomendado')
 });
-// Modifique o href do link externo no menu
-const href = document.querySelector('a[href^="http"]')
-href.setAttribute('href', 'https://nicollasalbert.github.io');
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+const menor = matchMedia("(max-width: 720px)");
+const menu = document.querySelector(".menu");
+
+if (menor.matches) {
+  menu.classList.add("menu-mobile");
+}
