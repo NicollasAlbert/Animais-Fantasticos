d60583acf4ae91deba91034d46d7ccf206e1f470
Nicollas <nicksz55@hotmail.com>
2023-01-28 12:35:45 -0300

    Estudos JavaScript no Front-End

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const imgDistancia = document.querySelector("img");
-console.log(imgDistancia.offsetTop);
-const imgRect = imgDistancia.getBoundingClientRect().top;
-console.log(imgRect);
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const links = document.querySelectorAll(".menu a[href^='#']");
 
-// Retorne a soma da largura de todas as imagens
-function somaImagens() {
-  const imgSoma = document.querySelectorAll("img");
-  let soma = 0;
-  imgSoma.forEach(function (img) {
-    const imgTotal = img.offsetWidth;
-    soma += img.offsetWidth;
-  });
-  console.log(soma);
-}
-
-window.onload = function () {
-  somaImagens();
-};
-
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('a');
 links.forEach((link) => {
-  linkWidth = link.offsetWidth;
-  linkHeight = link.offsetHeight;
-  if(linkWidth >= 48 && linkHeight >= 48)
-  console.log(link, 'Os links estão cumprindo o recomendado')
-  else
-  console.log(link, 'Os links não estão cumprindo o recomendado')
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    link.classList.toggle("ativo");
+  });
 });
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const menor = matchMedia("(max-width: 720px)");
-const menu = document.querySelector(".menu");
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const body = document.querySelectorAll("body *");
 
-if (menor.matches) {
-  menu.classList.add("menu-mobile");
+function aoClicar(e) {
+  const evento = e.target;
+  console.log(evento);
 }
+
+body.forEach((itens) => {
+  itens.addEventListener("click", aoClicar);
+});
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
