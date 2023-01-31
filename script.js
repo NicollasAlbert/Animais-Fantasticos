e549a5844535e16e3d9993d8ea82e7ac0b64b8f3
Nicollas <nicksz55@hotmail.com>
2023-01-31 11:58:30 -0300

    Finalização site com JavaScript no Front

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+const activeClass = "ativo";
+
 function initTabNav() {
   const menu = document.querySelectorAll(".js-tabmenu li");
   const content = document.querySelectorAll(".js-tabcontent section");
 
   if (menu.length && content.length) {
-    content[0].classList.add("ativo");
+    content[0].classList.add(activeClass);
 
     function menuContent(index) {
       content.forEach((item) => {
-        item.classList.remove("ativo");
+        item.classList.remove(activeClass);
       });
-      content[index].classList.add("ativo");
+      content[index].classList.add(activeClass);
     }
 
     menu.forEach((itemMenu, index) => {
@@ -24,7 +26,6 @@ initTabNav();
 
 function initAccordion() {
   const listaAccordion = document.querySelectorAll(".js-accordion dt");
-  const activeClass = "ativo";
 
   if (listaAccordion.length) {
     listaAccordion[0].classList.add(activeClass);
@@ -42,3 +43,53 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSmooth() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function slideSmooth(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // scrollTo({
+    //   top: topo,
+    //   behavior: "smooth",
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", slideSmooth);
+  });
+}
+
+initScrollSmooth();
+
+function initScrollAnimado() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowsMetade = window.innerHeight * 0.5;
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowsMetade < 0;
+        if (isSectionVisible) section.classList.add(activeClass);
+        else section.classList.remove(activeClass);
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initScrollAnimado();
