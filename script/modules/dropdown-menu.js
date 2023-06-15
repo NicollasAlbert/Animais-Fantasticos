f5c9d8ca1c0fef61e44132181af2f3391c4ddf85
NicollasAlbert <nicksz55@hotmail.com>
2023-06-15 16:41:08 -0300

    Adicionando Drop Down Menu

---
+++ script/modules/dropdown-menu.js
@@ -0,0 +1,16 @@
+export default function initDropDownMenu() {
+
+}
+
+const dropDownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropDownMenus.forEach(menu => {    
+    ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick);
+    })
+});
+
+function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('ativo');
+}
