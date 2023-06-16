426f7ccc1e25fa3dbaa78a13f5e14709c7f9c618
NicollasAlbert <nicksz55@hotmail.com>
2023-06-16 15:56:55 -0300

    Adicionando outside clck

--- script/modules/dropdown-menu.js
+++ script/modules/dropdown-menu.js
@@ -1,16 +1,20 @@
+import outsideClick from "./outsideclick";
+
 export default function initDropDownMenu() {
 
-}
+    const dropDownMenus = document.querySelectorAll('[data-dropdown]');
 
-const dropDownMenus = document.querySelectorAll('[data-dropdown]');
+    dropDownMenus.forEach(menu => {    
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        })
+    });
 
-dropDownMenus.forEach(menu => {    
-    ['touchstart', 'click'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick);
-    })
-});
-
-function handleClick(event) {
-    event.preventDefault();
-    this.classList.toggle('ativo');
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('ativo');
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('ativo');
+        });
+    }
 }
